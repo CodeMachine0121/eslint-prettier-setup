3dec28dd597578301295424fe63ac9ed4af8a74f
james.hsueh77 <james.hsueh@titansoft.com.sg>
2024-03-25 00:13:30 +0800

    add format command consider ts file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+const a = "test";
+console.log(a);
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
